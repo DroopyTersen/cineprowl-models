a2bf66f2c10dc1a3bb7d8e50f5971826082296c8
Andrew Petersen <andrew.petersen15@gmail.com>
2014-02-13 19:07:56 -0600

    got everything working on Ratburn and added NasUrl

--- FullMovie.js
+++ FullMovie.js
@@ -6,12 +6,14 @@ module.exports = {
 		movie.backgroundUrl = imageHelper.backdrop.getMid(movie.backdrop_path);
 		movie.posterUrl = imageHelper.poster.getMid(movie.poster_path);
 		movie.casts.cast.forEach(function(cast){
-			console.log(cast.profile_path)
 			cast.profileUrl = imageHelper.profile.getThumb(cast.profile_path);
 		});
 		movie.watched = movie.watched === true ? "Watched" : "Unwatched";
 		var date = new Date(movie.release_date);
 		movie.release_date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+		movie.nasUrl = "https://runwatcher.com/manage/shares/Movies/";
+		movie.nasUrl += movie.file.library.replace("\\\\ix4-300d\\movies\\", "");
+		movie.nasUrl += "/" + movie.file.parentFolder + "/" + movie.file.filename;
 		return movie;
 	}
 };
